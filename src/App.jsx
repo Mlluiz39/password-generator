cfd63e3e57a1e71d107cfb0042ad5d906987d04c
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-09-24 17:28:22 -0300

    add new functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import Logo from '@/assets/logo.png'
-import { LowercaseLetter } from '@/components/LowercaseLetter'
-import { UppercaseLetter } from './components/UppercaseLetter'
-import { NumbersInput } from './components/NumbersInput'
-import { SymbolsInput } from './components/SymbolsInput'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dataValue, setDataValue] = useState('')
+  const [count, setCount] = useState(6)
+  const [lowercaseLetter, setLowercaseLetter] = useState('')
+  const [uppercaseLetter, setUppercaseLetter] = useState('')
+  const [number, setNumber] = useState('')
+  const [symbol, setSymbol] = useState('')
+
+  const handleLowercaseLetter = e => {
+    const alphabet = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    setLowercaseLetter(e.target.checked ? alphabet : '')
+  }
+
+  const handleUppercaseLetter = e => {
+    const alphabet = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    setUppercaseLetter(e.target.checked ? alphabet : '')
+  }
+
+  const handleNumber = e => {
+    const number = Math.floor(Math.random() * 10)
+    setNumber(e.target.checked ? number : '')
+  }
+
+  const handleSymbol = e => {
+    const symbol = String.fromCharCode(Math.floor(Math.random() * 15) + 33)
+    setSymbol(e.target.checked ? symbol : '')
+  }
+
+  const handleGenerate = () => {
+    const data = lowercaseLetter + uppercaseLetter + number + symbol
+    setDataValue(data)
+  }
+
+  console.log(dataValue)
 
   return (
     <main className="p-10 m-4 shadow-2xl shadow-indigo-900">
@@ -28,13 +56,56 @@ function App() {
           {count}
         </label>
         <div className="flex flex-col">
-          <LowercaseLetter />
-          <UppercaseLetter />
-          <NumbersInput />
-          <SymbolsInput />
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                className="mx-2 border-none"
+                checked={lowercaseLetter}
+                onChange={handleLowercaseLetter}
+              />
+            </label>
+            Letras maiúsculas
+          </div>
+          <div>
+            <label className="pb-3">
+              <input
+                type="checkbox"
+                className="mx-2 border-none"
+                checked={uppercaseLetter}
+                onChange={handleUppercaseLetter}
+              />
+              Letras minúsculas
+            </label>
+          </div>
+          <div>
+            <label className="pb-3">
+              <input
+                type="checkbox"
+                className="mx-2 border-none"
+                checked={number}
+                onChange={handleNumber}
+              />
+              Números
+            </label>
+          </div>
+          <div>
+            <label className="pb-3">
+              <input
+                type="checkbox"
+                className="mx-2 border-none"
+                checked={symbol}
+                onChange={handleSymbol}
+              />
+              Símbolos
+            </label>
+          </div>
         </div>
         <div className="flex justify-center items-center mt-6">
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <button
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            onClick={handleGenerate}
+          >
             Gerar senha
           </button>
         </div>
